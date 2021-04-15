9db2965f9cdaafc7090ada8bd870a5e4920dadfb
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-04-14 22:54:06 -0700

    Feat: SAVE_BOOK mutations added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,34 @@ export const LOGIN_USER = gql`
     }
 `;
 // ADD_USER will execute the addUser mutation.
-
+export const ADD_USER = gql`
+    mutation addUser ($username: String!, $email: String!, $password: String!) {
+        addUser (username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
 // SAVE_BOOK will execute the saveBook mutation.
-
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookId: String!, $title: String!, $description: String, $authors: [String], $image: String, $link: String) {
+        saveBook(bookId: $bookId, title: $title, description: $description, authors: $authors, image: $image, link: $link) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
 // REMOVE_BOOK will execute the removeBook mutation.
