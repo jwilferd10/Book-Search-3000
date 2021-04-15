f1d527f005f0e480110ef6d8ea0d797891c430e0
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-04-14 22:43:32 -0700

    Feat: LOGIN_USER created

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,17 @@
-// mutations.js:
+import gql from 'graphql-tag';
 
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
-
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
 // ADD_USER will execute the addUser mutation.
 
 // SAVE_BOOK will execute the saveBook mutation.
