ca48dc053c4bc7ac7d5b58ae90deb33d1eaf9b9e
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-04-14 20:57:57 -0700

    Feat: Implemented apollo server and am currently applying it to express as a middleware

--- server/server.js
+++ server/server.js
@@ -3,8 +3,30 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Import Apollo-Server-Express
+const { ApolloServer } = require('apollo-server-express');
+// Import typedefs and resolvers, will be needed when created
+const { typeDefs, resolvers } = require('./schemas');
+// Import the authMiddleware for when it's modified
+const { authMiddleware } = require('./utils/auth');
+const auth = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Add the Apollo Server and pass in schema data here
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// Strike this off the to-do list
+// This intergrates the apollo server with the Express Application as a middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +36,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// For now, we may not be needing this.
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+
